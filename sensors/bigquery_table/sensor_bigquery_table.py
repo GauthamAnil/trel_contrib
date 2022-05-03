44004d1bead5cf0bbd3992917c1c86313ae5091e
Gautham Anil <gautham.anil@gmail.com>
2022-05-03 17:37:54 +0000

    Typo

--- sensors/bigquery_table/sensor_bigquery_table.py
+++ sensors/bigquery_table/sensor_bigquery_table.py
@@ -4,7 +4,7 @@ This sensor can monitor a given dataset in BigQuery to look for tables with an o
 
 E.g., table abc_20210201 will be cataloged as <dataset_class>,,20210201,<label>,<repository>
 
-bq_table_prefix has to contain the full prefix. E.g. abc_ for abc_20210201. Just abc or ab will not be enough.
+bq_table_prefix has to contain the full prefix. E.g. abc\_ for abc_20210201. Just abc or ab will not be enough.
 
 TODO: Add option to query using table metadata and not API. This would scale better.
 
